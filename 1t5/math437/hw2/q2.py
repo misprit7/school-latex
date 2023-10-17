b23f4fd8c7e5cf730f0f6191285df162cf1eaacb
misprit7 <xandernaumenko@gmail.com>
2023-10-17 01:28:35 -0700

    Started 437

---
+++ 1t5/math437/hw2/q2.py
@@ -0,0 +1,11 @@
+
+# for m in range(1, 1000):
+#     for n in range(1, 1000):
+#         if 2**m-3**n == 7:
+#             print(m,n)
+
+for k in range(0, 1000):
+    for l in range(0, 1000):
+        if 16*64**k-9*81**l == 7:
+            print(6*k+4,4*l+2)
+
