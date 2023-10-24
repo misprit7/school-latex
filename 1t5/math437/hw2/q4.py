cfe5456a0f20ffdc77c0e9b55d79a2dd6b029460
misprit7 <xandernaumenko@gmail.com>
2023-10-24 15:12:05 -0700

    Continued 437

--- 1t5/math437/hw2/q4.py
+++ 1t5/math437/hw2/q4.py
@@ -1,23 +1,36 @@
-from sympy import divisors, totient, factorial, N
+from sympy import divisors, totient, factorial, N, log
 import matplotlib.pyplot as plt
 
-a = []
-f = []
-d = []
-p = []
-for n in range(1, 20):
-    fac = factorial(n)
-    a += [fac/len(divisors(fac))/totient(fac)]
-    f += [fac]
-    d += [len(divisors(fac))]
-    p += [totient(fac)]
+# a = []
+# f = []
+# d = []
+# p = []
+# for n in range(1, 20):
+#     fac = factorial(n)
+#     a += [fac/len(divisors(fac))/totient(fac)]
+#     f += [fac]
+#     d += [len(divisors(fac))]
+#     p += [totient(fac)]
+
+# for i in range(30):
+#     if i > 2:
+#         print(i, d[-1], N(2**(d[-1]-d[-2])))
+# exit()
 
 b = []
-# for n in range(1, 15):
-#     b += [N(factorial(n)/2**(len(divisors(factorial(n)))))]
+d = []
+for n in range(1, 26):
+    b += [N(factorial(n)/2**(len(divisors(factorial(n)))))]
+    d += [len(divisors(factorial(n)))]
+    if n > 2:
+        print(n, d[-1], d[-1]-d[-2], len([x for x in divisors(factorial(n-1)) if x%2 == 1 and x%3 == 0 and x%5==0]))
 
 # plt.semilogy(a)
 # plt.show()
-print(list(zip(a,f,d,p)))
+
+print(b)
+print([N(b[i+1]/b[i]) for i in range(len(b)-1)])
+
+# print(list(zip(a,f,d,p)))
 # print([N(a[i+1]/a[i]) for i in range(len(a)-1)])
 
