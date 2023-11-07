2b265ee1b22783c835b49fdeda1bf8cd82be0890
misprit7 <xandernaumenko@gmail.com>
2023-11-06 17:56:36 -0800

    406

--- 1t5/math406/hw4/q4.py
+++ 1t5/math406/hw4/q4.py
@@ -3,29 +3,27 @@ import numpy as np
 n=5
 
 A5 = np.zeros((n, n))
-G = np.zeros((n+1, n+1))
+G = np.zeros((n+2, n+2))
 
 for i in range(n):
     A5[i][i] = -2
     if i > 0:
         A5[i][i-1] = 1
-    if i < n-2:
+    if i < n-1:
         A5[i][i+1] = 1
-# A5[0] = 0
-# A5[n] = 0
 
-for i in range(0,n+1):
-    for j in range(0,n+1):
-        k = j*(j-n)/n
+for i in range(0,n+2):
+    for j in range(0,n+2):
+        k = j*(j-(n+1))/(n+1)
         if i < j:
             G[i][j] = k*i/j
         elif i == j:
             G[i][j] = k
         else:
-            G[i][j] = k*(n-i)/(n-j)
+            G[i][j] = k*((n+1)-i)/((n+1)-j)
 
-inv = np.linalg.inv(A5)
 print(A5)
+inv = np.linalg.inv(A5)
 print(inv)
 print(G)
 
